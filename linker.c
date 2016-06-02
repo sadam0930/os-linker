6809ca4f271d43db51de3ba4d68555aa1d138473
Steven N Adam <sna219@nyu.edu>
2016-06-02 15:58:06 -0400

    add read file

--- linker.c
+++ linker.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 	//get filename
 	char filename[255];
-	prinf("Enter filename: ");
+	printf("Enter filename: ");
 	scanf("%s", filename);
-	
+	//printf("%s\n", filename); //debug
+
 	//read file character by character
-	prinf("%s\n", filename); //test
+	FILE *fp;
+	fp = fopen(filename, "r");
+	if(fp == NULL) {
+		printf("Cannot open file: %s\n", filename);
+		exit(1);
+	}
+
+	char nextChar;
+	while(fscanf(fp, "%c", &nextChar) != EOF) {
+		//printf("%c", nextChar); //debug
+	}
+
+	fclose(fp);
+
 
 	return 0;
-}
+}	
